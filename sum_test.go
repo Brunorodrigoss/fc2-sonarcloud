47cb570b141b96694cb492357ab15997f15db2f8
Bruno Rodrigo <bnorodrigo@gmail.com>
2022-09-17 23:00:29 +0100

    add tests to pass in quality gate

--- sum_test.go
+++ sum_test.go
@@ -9,3 +9,27 @@ func TestSum(t *testing.T) {
 		t.Error("The result must be 5")
 	}
 }
+
+func TestSub(t *testing.T) {
+	result := sub(2, 2)
+
+	if result != 0 {
+		t.Error("The result must be 0")
+	}
+}
+
+func TestTimes(t *testing.T) {
+	result := sub(2, 2)
+
+	if result != 4 {
+		t.Error("The result must be 4")
+	}
+}
+
+func TestSumX(t *testing.T) {
+	result := sub(2, 2)
+
+	if result != 6 {
+		t.Error("The result must be 6")
+	}
+}
